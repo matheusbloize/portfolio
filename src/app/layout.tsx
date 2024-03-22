295916e74580cf68203b917c3ed9ccbc2311a6c0
Matheus Bloize <mbloizepd@gmail.com>
2024-03-22 16:33:33 -0300

    feat: project started

---
+++ src/app/layout.tsx
@@ -0,0 +1,46 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const clashGrotesk = localFont({
+  src: "../assets/fonts/ClashGrotesk-Variable.ttf",
+});
+
+export const metadata: Metadata = {
+  title: "Matheus Bloize - Frontend Developer",
+  description:
+    "Matheus Bloize is a chill frontend developer who builds easy and accessible interfaces.",
+  authors: [
+    {
+      name: "Matheus Bloize",
+      url: "https://www.linkedin.com/in/matheus-bloize/",
+    },
+  ],
+  openGraph: {
+    title: "Matheus Bloize - Frontend Developer",
+    description:
+      "Matheus Bloize is a chill frontend developer who builds easy and accessible interfaces.",
+  },
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${clashGrotesk.className} bg-mb-base text-mb-black flex flex-col`}
+      >
+        {children}
+        <svg>
+          <filter id="grainy">
+            <feTurbulence type="turbulence" baseFrequency=".25"></feTurbulence>
+            <feColorMatrix type="saturate" values="0"></feColorMatrix>
+          </filter>
+        </svg>
+      </body>
+    </html>
+  );
+}
---
+++ tailwind.config.ts
@@ -0,0 +1,42 @@
+import type { Config } from "tailwindcss";
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      fontFamily: {
+        clash: ["Clash Grotesk", "system-ui"],
+        general: ["General Sans", "system-ui"],
+      },
+      colors: {
+        "mb-base": "#F1E7DB",
+        "mb-black": "#131315",
+        "mb-white": "#FBFAFB",
+        "mb-green": "#5BDE0A",
+      },
+      fontSize: {
+        128: "8em",
+        96: "6em",
+        64: "4em",
+        48: "3em",
+        36: "2.25em",
+        32: "2em",
+        24: "1.5em",
+        20: "1.25em",
+        16: "1em",
+        14: "0.875em",
+        12: "0.75em",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
